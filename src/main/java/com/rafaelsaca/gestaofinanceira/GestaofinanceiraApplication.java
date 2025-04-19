1137aeda9e68c3688bb890e20ef9dd31d6ade509
RafaelSaCa <raffaellsaca@gmail.com>
2025-04-19 15:09:22 -0300

    commit inicial

---
+++ src/main/java/com/rafaelsaca/gestaofinanceira/GestaofinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsaca.gestaofinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaofinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaofinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelsaca/gestaofinanceira/GestaofinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsaca.gestaofinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaofinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
