267dff9f8d15a2fccc339adedadd2597babd5769
RafaelSaCa <raffaellsaca@gmail.com>
2025-04-23 14:30:41 -0300

    feat: Model Categoria

---
+++ src/main/java/com/rafaelsaca/gestaofinanceira/models/Categoria.java
@@ -0,0 +1,61 @@
+package com.rafaelsaca.gestaofinanceira.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "categoria")
+    private List<Transacao> transacoes = new ArrayList<>();
+
+    public Categoria(Long id, String descricao, List<Transacao> transacoes) {
+        this.id = id;
+        this.descricao = descricao;
+        this.transacoes = transacoes;
+    }
+
+    public Categoria() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<Transacao> transacoes) {
+        this.transacoes = transacoes;
+    }
+
+}
