cb71786732fd0e55865b410f478f43fc30be6a74
RafaelSaCa <raffaellsaca@gmail.com>
2025-04-25 13:47:32 -0300

    feat: Model transação

---
+++ src/main/java/com/rafaelsaca/gestaofinanceira/models/Transacao.java
@@ -0,0 +1,131 @@
+package com.rafaelsaca.gestaofinanceira.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_transacao")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    private TipoTransacao tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(updatable = false, name = "data_criacao") // updatable evita que esse campo seja alterado
+    private LocalDate criadoEm;
+
+    @PrePersist
+    public void prePersist() {
+        this.criadoEm = LocalDate.now(); // seta automaticamente antes de salvar
+    }
+
+    public Transacao() {
+    }
+
+    public Transacao(Long id, String descricao, BigDecimal valor, TipoTransacao tipo, Categoria categoria,
+            LocalDate dataVencimento, LocalDate criadoEm, Usuario usuario) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.dataVencimento = dataVencimento;
+        this.criadoEm = criadoEm;
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public TipoTransacao getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoTransacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDate getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDate criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+}
