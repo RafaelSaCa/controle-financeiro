fb2341d130db7e3edb6190cd5468fc656ff51030
RafaelSaCa <raffaellsaca@gmail.com>
2025-04-27 20:07:56 -0300

    feat: Transacao Mapper - converte uma lista de transacoes model para lista dto

--- src/main/java/com/rafaelsaca/gestaofinanceira/mappers/TransacaoMapper.java
+++ src/main/java/com/rafaelsaca/gestaofinanceira/mappers/TransacaoMapper.java
@@ -1,14 +1,19 @@
 package com.rafaelsaca.gestaofinanceira.mappers;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import com.rafaelsaca.gestaofinanceira.dto.TransacaoDTO;
 import com.rafaelsaca.gestaofinanceira.models.Categoria;
 import com.rafaelsaca.gestaofinanceira.models.TipoTransacao;
 import com.rafaelsaca.gestaofinanceira.models.Transacao;
 import com.rafaelsaca.gestaofinanceira.models.Usuario;
 
+@Component
 public class TransacaoMapper {
 
-    public static Transacao toModel (TransacaoDTO dto){
+    public Transacao toModel (TransacaoDTO dto){
         Transacao transacao = new Transacao();
         
         transacao.setDescricao(dto.descricao());
@@ -29,7 +34,7 @@ public class TransacaoMapper {
     }
 
 
-    public static TransacaoDTO toDTO (Transacao transacao){
+    public TransacaoDTO toDTO (Transacao transacao){
         return new TransacaoDTO(
             transacao.getDescricao(), 
             transacao.getValor(),
@@ -38,6 +43,12 @@ public class TransacaoMapper {
             transacao.getUsuario().getId(),
             transacao.getDataVencimento()
         );
+    }   
+
+    public List<TransacaoDTO> toListDTO (List<Transacao> transacoes){
+        return transacoes.stream()
+                         .map(this::toDTO)
+                         .toList();
     }
 
 }
