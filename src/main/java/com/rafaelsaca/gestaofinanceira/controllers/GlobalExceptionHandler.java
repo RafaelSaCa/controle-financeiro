0bc6c2bad52897b052d8e4309e7b0ca4ae0b53a2
RafaelSaCa <raffaellsaca@gmail.com>
2025-04-27 20:03:31 -0300

    feat: handler para verificar o tipo do parametro enviado.

--- src/main/java/com/rafaelsaca/gestaofinanceira/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/rafaelsaca/gestaofinanceira/controllers/GlobalExceptionHandler.java
@@ -1,38 +1,58 @@
 package com.rafaelsaca.gestaofinanceira.controllers;
 
+import java.net.URI;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.rafaelsaca.gestaofinanceira.exceptions.AppException;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-           @ExceptionHandler(AppException.class)
-        public ProblemDetail handleAppException(AppException e){
+        //retornar erros com um formato mais padronizado, estilo RFC 7807 
+        //(Problem Details for HTTP APIs)
+
+        @ExceptionHandler(AppException.class)
+        public ProblemDetail handleAppException(AppException e) {
                 return e.toProblemDetail();
         }
 
-
         @ExceptionHandler(MethodArgumentNotValidException.class)
-        public ProblemDetail handleMethodArgumentNotValidException( MethodArgumentNotValidException e){
-                
+        public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
                 var fieldErrors = e.getFieldErrors()
-                        .stream()
-                        .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
-                        .toList();
+                                .stream()
+                                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                                .toList();
 
                 var problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
 
                 problemDetail.setDetail("Os dados enviados não são válidos!");
-                problemDetail.setProperty("parametros-invalidos",  fieldErrors);
+                problemDetail.setProperty("parametros-invalidos", fieldErrors);
 
                 return problemDetail;
         }
 
-        private record InvalidParam(String campo, String motivo){}
+        private record InvalidParam(String campo, String motivo) {
+        }
+
+        //verifica se enviar um ID inválido (tipo string e nao long)
+        @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+        public ResponseEntity<ProblemDetail> handleInvalidPathParam(MethodArgumentTypeMismatchException ex,
+                        HttpServletRequest request) {
+                ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+                problem.setTitle("Verifique os dados!");
+                problem.setDetail("O tipo do parâmetro é inválido: " + ex.getName());
+                problem.setInstance(URI.create(request.getRequestURI()));
+                return ResponseEntity.badRequest().body(problem);
+        }
 
 }
