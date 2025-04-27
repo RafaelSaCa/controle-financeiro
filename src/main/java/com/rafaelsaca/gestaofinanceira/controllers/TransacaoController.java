c0019728d09ea7a0ab5151c828cf20498dbe2778
RafaelSaCa <raffaellsaca@gmail.com>
2025-04-27 20:17:25 -0300

    feat: Controller Transacao - atualizacao e busca de transacao por usuario

--- src/main/java/com/rafaelsaca/gestaofinanceira/controllers/TransacaoController.java
+++ src/main/java/com/rafaelsaca/gestaofinanceira/controllers/TransacaoController.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelsaca.gestaofinanceira.dto.TransacaoDTO;
+import com.rafaelsaca.gestaofinanceira.dto.TransacaoResponse;
 import com.rafaelsaca.gestaofinanceira.models.Transacao;
 import com.rafaelsaca.gestaofinanceira.services.TransacaoService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/transacoes")
@@ -40,9 +44,16 @@ public class TransacaoController {
         return ResponseEntity.ok(service.listar());
     }
 
-    @GetMapping("/usuario/{id}")
-    public ResponseEntity<List<Transacao>> buscaTransacoesPorUsuario(@PathVariable Long id) {
-        List<Transacao> transacoes = service.buscaTransacoesPorUsuario(id);
+    // @GetMapping("/usuario/{id}")
+    // public ResponseEntity<List<Transacao>> buscaTransacoesPorUsuario(@PathVariable Long id) {
+    //     List<Transacao> transacoes = service.buscaTransacoesPorUsuario(id);
+
+    //     return ResponseEntity.ok(transacoes);
+    // }
+
+    @GetMapping("/usuario")
+    public ResponseEntity<TransacaoResponse> transacoesPorUsuario(@RequestParam @NotNull Long usuarioId) {
+        TransacaoResponse transacoes = service.transacoesPorUsuario(usuarioId);
 
         return ResponseEntity.ok(transacoes);
     }
@@ -52,6 +63,19 @@ public class TransacaoController {
         List<Transacao> transacoes = service.buscaPorTipo(tipo);
 
         return ResponseEntity.ok(transacoes);
+    }
+
+    @PutMapping("/{transacaoId}")
+    public ResponseEntity<Transacao> atualizar(@PathVariable  @NotNull Long transacaoId,
+            @RequestBody @Valid TransacaoDTO dto) {
+
+        return ResponseEntity.ok(service.atualizar(transacaoId, dto));
+    }
+
+    @DeleteMapping("/{transacaoId}")
+    public ResponseEntity<Void> deletar (@PathVariable @NotNull Long transacaoId){
+        service.deletar(transacaoId);
+        return ResponseEntity.noContent().build();
     }
 
 }
