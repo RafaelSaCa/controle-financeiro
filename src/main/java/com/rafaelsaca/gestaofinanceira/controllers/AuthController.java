7d1a537b5caabc5186afc50a5848aa78d8eca43f
RafaelSaCa <raffaellsaca@gmail.com>
2025-05-06 22:15:34 -0300

    feat: Rota para cadastro de nova conta

--- src/main/java/com/rafaelsaca/gestaofinanceira/controllers/AuthController.java
+++ src/main/java/com/rafaelsaca/gestaofinanceira/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.rafaelsaca.gestaofinanceira.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelsaca.gestaofinanceira.dto.LoginDTO;
 import com.rafaelsaca.gestaofinanceira.dto.TokenDTO;
+import com.rafaelsaca.gestaofinanceira.dto.UsuarioDTO;
+import com.rafaelsaca.gestaofinanceira.models.Usuario;
 import com.rafaelsaca.gestaofinanceira.services.AuthService;
+import com.rafaelsaca.gestaofinanceira.services.UsuarioService;
 
 import jakarta.validation.Valid;
 
@@ -17,14 +21,22 @@ import jakarta.validation.Valid;
 public class AuthController {
 
     private final AuthService authService;
+    private final UsuarioService usuarioService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, UsuarioService usuarioService) {
         this.authService = authService;
+        this.usuarioService = usuarioService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
         String token = authService.login(loginDTO.email(), loginDTO.senha());
         return ResponseEntity.ok(new TokenDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Usuario> cadastrar(@RequestBody @Valid UsuarioDTO dto) {
+        Usuario usuario = usuarioService.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 }
