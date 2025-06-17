5938d0b0fae7c6b4a86ecfbf2acb4829832f518b
RafaelSaCa <raffaellsaca@gmail.com>
2025-06-16 22:23:44 -0300

    feat: TransacaoDTO

--- src/main/java/com/rafaelsaca/gestaofinanceira/dto/TransacaoDTO.java
+++ src/main/java/com/rafaelsaca/gestaofinanceira/dto/TransacaoDTO.java
@@ -7,11 +7,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record TransacaoDTO( @NotBlank String descricao,
-                            @NotNull @Positive BigDecimal valor,
-                            @NotBlank String tipo, 
-                            @NotNull Long categoriaId,
-                            @NotNull Long usuarioId,
-                            @NotNull LocalDate dataVencimento) {
+public record TransacaoDTO(@NotBlank String descricao,
+        @NotNull @Positive BigDecimal valor,
+        @NotBlank String tipo,
+        @NotNull Long categoriaId,
+        @NotNull LocalDate dataVencimento) {
 
 }
