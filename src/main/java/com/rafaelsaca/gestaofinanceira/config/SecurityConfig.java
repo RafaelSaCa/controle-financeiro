2980658724c54dd5ff13c3439adba99df246faa5
RafaelSaCa <raffaellsaca@gmail.com>
2025-06-27 22:08:07 -0300

    cors config - acesso ao domínio de produção

--- src/main/java/com/rafaelsaca/gestaofinanceira/config/SecurityConfig.java
+++ src/main/java/com/rafaelsaca/gestaofinanceira/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://controle-financeiro-tpo6.onrender.com"));//http://localhost:4200
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
